aab6e3f65b14cd3924bb9b29e8d12980d55bea68
Jordan Sue <jasyvr@gmail.com>
2024-02-29 15:04:49 -0800

    Fixed merge errors

--- blackjack.hs
+++ blackjack.hs
@@ -78,8 +78,8 @@ blackjack Stand (State (playerHand, computerHand, dealerHand, deck) bet cbet cun
 checkBust :: State -> Result
 checkBust (State (playerHand, computerHand, dealerHand, deck) bet cbet cunit count turn pStand cStand dStand pBust cBust)
     | getHandValue dealerHand > 21 = EndOfGame (State (playerHand, computerHand, dealerHand, deck) bet cbet cunit count turn pStand 1 dStand pBust cBust) 2
-    | getHandValue playerHand > 21 && cBust == 1 = EndOfGame (State (playerHand, computerHand, dealerHand, deck) bet cbet cunit count turn pStand 1 dStand pBust cBust) 4
-    | getHandValue computerHand > 21 && pBust == 1 = EndOfGame (State (playerHand, computerHand, dealerHand, deck) bet cbet cunit count turn pStand 1 dStand pBust cBust) 4
+    | getHandValue playerHand > 21 && cBust == 1 = EndOfGame (State (playerHand, computerHand, dealerHand, deck) bet cbet cunit count turn pStand 1 dStand 1 cBust) 4
+    | getHandValue computerHand > 21 && pBust == 1 = EndOfGame (State (playerHand, computerHand, dealerHand, deck) bet cbet cunit count turn pStand 1 dStand pBust 1) 4
     | getHandValue playerHand > 21 = ContinueGame (State (playerHand, computerHand, dealerHand, deck) bet cbet cunit count turn 1 cStand dStand 1 cBust)
     | getHandValue computerHand > 21 = ContinueGame (State (playerHand, computerHand, dealerHand, deck) bet cbet cunit count turn pStand 1 dStand pBust 1)
     | otherwise = ContinueGame (State (playerHand, computerHand, dealerHand, deck) bet cbet cunit count turn pStand cStand dStand pBust cBust)
@@ -181,10 +181,11 @@ personPlay game (ContinueGame state) balances = let (State (playerHand, computer
         putStrLn "That is not a valid input."
         personPlay game (ContinueGame state) balances
 
+-- delegates the EndofGame tasks to the dealer
 personPlay game (EndOfGame state lost) (x,y) = let (State (playerHand, computerHand, dealerHand, deck) bet cbet cunit count turn pStand cStand dStand pBust cBust) = state in do
     dealerPlay game (EndOfGame state lost) (x,y)
 
--- Determines whether standing or hitting would be optimal
+-- decides whether or not the computer should stand based on other hands
 shouldStand :: (Ord a1, Ord a2, Num a3, Num a1, Num a2, Eq a3) => a3 -> a1 -> a2 -> Bool
 shouldStand aces handVal upCard = do
     if aces == 0 then do
@@ -224,6 +225,7 @@ computerPlay game (ContinueGame state) balances = let (State (playerHand, comput
         else 
             dealerPlay game (game Hit state) balances
 
+-- delegates the EndofGame tasks to the dealer
 computerPlay game (EndOfGame state lost) (x,y) = let (State (playerHand, computerHand, dealerHand, deck) bet cbet cunit count turn pStand cStand dStand pBust cBust) = state in do
     dealerPlay game (EndOfGame state lost) (x,y)
 
@@ -275,14 +277,7 @@ dealerPlay game (EndOfGame state lost) (x,y) = let (State (playerHand, computerH
             play game (State ([], [], [], deck) 0 0 cunit count 0 0 0 0 0 0) (x + bet, y + cbet) 
     else if pBust == 1 && cBust == 1 then do
         putStrLn "\nPlayer and Computer Bust, Dealer Wins"
-        if checkIfOutOfMoney (y - cbet) && checkIfOutOfMoney (x - bet) then do
-            bothOutOfMoneyStatement (x - bet, y - cbet)
-        else if checkIfOutOfMoney (y - cbet) then do
-            computerOutOfMoneyStatement (x - bet, y - cbet)
-        else if checkIfOutOfMoney (x - bet) then do 
-            playerOutOfMoneyStatement (x - bet, y - cbet)
-        else do
-            play game (State ([], [], [], deck) 0 0 cunit count 0 0 0 0 0 0) (x - bet, y - cbet)
+        dealerBeatBothStatement game state (x,y)
     else if pBust == 1 then do
         putStrLn "\nPlayer Bust"
         if getHandValue computerHand > getHandValue dealerHand then do
@@ -369,6 +364,7 @@ checkIfOutOfMoney balance
     | balance == 0 = True
     | otherwise = False
 
+-- output the proper statement if the dealer beat both computer and player
 dealerBeatBothStatement :: (Action -> State -> Result) -> State -> Balances -> IO Balances
 dealerBeatBothStatement game state (x,y) = let (State (playerHand, computerHand, dealerHand, deck) bet cbet cunit count turn pStand cStand dStand pBust cBust) = state in do
     if checkIfOutOfMoney (y - cbet) && checkIfOutOfMoney (x - bet) then do
@@ -439,9 +435,14 @@ getDeposit = do
     else
         return balance
 
+-------------------- Test Cases ---------------------------
+
 -- dealerPlay blackjack (ContinueGame `state`) (100,100)
 allTie :: State
 allTie = State ([('h',10),('h',10)], [('h',10),('h',10)], [('h',10),('h',10)], [('h',6)]) 10 10 1 0 2 1 1 0 0 0
+
+allStandWin :: State
+allStandWin = State ([('h',10),('h',10)], [('h',10),('h',10)], [('h',10),('h',9)], [('h',6)]) 10 10 0 2 1 1 0 0 0
 
 allStandAllLose :: State
 allStandAllLose = State ([('h',10),('h',9)], [('h',10),('h',9)], [('h',10),('h',10)], [('h',6)]) 10 10 1 0 2 1 1 0 0 0
